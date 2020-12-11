abee9c0aaffd6a01be679631a4105a42e3abd6e6
Sarah Abdelfattah <sarah_23s@hotmail.com>
2020-12-12 00:06:21 +0200

    basic login function

--- server/controllers/staffMemberController.js
+++ server/controllers/staffMemberController.js
@@ -1,3 +1,43 @@
-const ObjectId = require('mongoose').Types.ObjectId;
+const objectId = require('mongoose').Types.ObjectId;
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const tokenKey = require('../config/keys').secretOrKey;
+// const { convertMapToObject } = require('../utils/convertMapToObject');
 // const { handleError } = require("../utils/handleError");
-// required models
+
+
+exports.login = async function (req, res, next) {
+    const guc_id = req.body.username;
+    const password = req.body.password;
+
+    try {
+        //check staff entered both
+        if (!guc_id || !password)
+            return res.send({ error: "please enter both your email and password" })
+
+        // check member id is found
+        const member = staffMember.findOne({ GUCID: guc_id })
+            res.status(400).json({ error: 'Email does not exist' });
+
+        //check password
+        const correctPassword = bcrypt.compareSync(password, staffMember.password);
+        if (!correctPassword) {
+            return res.status(400).send({ error: 'Invalid Password' });
+        } else {
+            //login
+            const payload = {
+                GUCID: staffMember.GUCID,
+                name: staffMember.name,
+                email: staffMember.email,
+                type: staffMember.type
+            }
+
+            const token = jwt.sign(payload, tokenKey, { expiresIn: '24h' })
+            return res.json({ data: `Bearer ${token}` })
+        }
+
+    } catch (err) {
+        res.json({ error: err })
+    }
+};
--- server/routes/staffMembers.js
+++ server/routes/staffMembers.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const staffMember = require('../models/StaffMember')
 
 //const { auth } = require("../../utils/authentication");
+
+router.post("/login", staffMemberController.login);
+
+
+module.exports = router;  
