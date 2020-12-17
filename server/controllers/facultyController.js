96e77f2966395935abeefe400314a8b1c5dd6879
Sarah Abdelfattah <sarah_23s@hotmail.com>
2020-12-17 11:25:10 +0200

    faculty

--- server/controllers/facultyController.js
+++ server/controllers/facultyController.js
@@ -4,8 +4,6 @@ const objectId = require('mongoose').Types.ObjectId;
 
 const StaffMember = require('../models/StaffMember');
 const Faculty = require('../models/Faculty');
-const DepartmentSchema = require('../models/schemas/Department');
-
 
 exports.addFaculty = async function (req, res) {
     try {
@@ -44,17 +42,21 @@ exports.updateFaculty = async function (req, res) {
         const name = req.body.name;
         const newName = req.body.newName;
 
-        // if (!name)
-        //     return res.send({ error: "Please enter the name of the faculty" });
+        if (!name || !newName)
+            return res.send({ error: "Please enter all details" });
 
-        // const facultyFound = await Faculty.findOne({ name: name });
-        // if (!facultyFound || !facultyFound.is_deleted == true)
-        //     return res.send({ error: "Sorry no faculty with this name" });
+        const facultyFound = await Faculty.findOne({ name: name, is_deleted: false });
+        if (!facultyFound)
+            return res.send({ error: "No faculty with this name" });
 
-        // facultyFound.is_deleted = true;
+        const nameFacultyFound = await Faculty.findOne({ name: name, is_deleted: false });
+        if (nameFacultyFound)
+            return res.send({ error: "Sorry there is a new faculty with this name" });
 
-        // await facultyFound.save();
-        // return res.send({ data: "Faculty deleted successfully " });
+        facultyFound.name = newName;
+
+        const updatedFaculty = await facultyFound.save();
+        return res.send({ data: updatedFaculty });
     } catch (err) {
         console.log('~ err', err);
         return res.send({ err: err });
@@ -86,19 +88,26 @@ exports.addDepartment = async function (req, res) {
     try {
         const { faculty, name, HOD } = req.body;
 
+        //all data entered
         if (!faculty || !name || !HOD)
             return res.send({ error: "Please enter all details" });
 
+        //faculty found? 
         const facultyFound = await Faculty.findOne({ name: faculty, is_deleted: false })
         if (!facultyFound || facultyFound.is_deleted === true)
             return res.send({ error: "No faculty with this name" });
 
+        // staff found? 
         const staffMember = await StaffMember.findOne({ gucId: HOD, is_deleted: false })
         if (!staffMember || staffMember.is_deleted)
             return res.send({ error: "No staff member with this ID" });
 
+        //staff is not TA and not HR 
         if (staffMember.role === 'Teaching Assistant' || staffMember.type === 'HR')
             return res.send({ error: "Sorry Head of the department cannot be Teaching Assistant or HR member" });
+
+        if (!(staffMember.faculty.equals(facultyFound._id)))
+            return res.send({ error: "Sorry Head of the department should be of the same faculty" });
 
         const allDep = facultyFound.departments
         for (var i in allDep) {
@@ -121,6 +130,54 @@ exports.addDepartment = async function (req, res) {
         const newFaculty = await facultyFound.save();
 
         return res.send({ data: newFaculty });
+    } catch (err) {
+        console.log('~ err', err);
+        return res.send({ err: err });
+    }
+}
+
+exports.updateDepartment = async function (req, res) {
+    try {
+        const { faculty, department, HOD } = req.body;
+
+        //all data entered
+        if (!faculty || !name || !HOD)
+            return res.send({ error: "Please enter all details" });
+
+        //faculty found? 
+        const facultyFound = await Faculty.findOne({ name: faculty, is_deleted: false })
+        if (!facultyFound || facultyFound.is_deleted === true)
+            return res.send({ error: "No faculty with this name" });
+
+        // staff found? 
+        const staffMember = await StaffMember.findOne({ gucId: HOD, is_deleted: false })
+        if (!staffMember || staffMember.is_deleted)
+            return res.send({ error: "No staff member with this ID" });
+
+        //staff is not TA and not HR 
+        if (staffMember.role === 'Teaching Assistant' || staffMember.type === 'HR')
+            return res.send({ error: "Sorry Head of the department cannot be Teaching Assistant or HR member" });
+
+        //staff of the same faculty?
+        if (!(staffMember.faculty.equals(facultyFound._id)))
+            return res.send({ error: "Sorry Head of the department should be of the same faculty" });
+
+
+        let found = false;
+        let allDep = facultyFound.departments
+        for (var i in allDep) {
+            if (allDep[i].name == department && allDep.is_deleted == false) {
+                found = true;
+                facultyFound.departments[i].HOD = staffMember._id;
+
+                const updatedHOD = await facultyFound.save();
+                return res.send({ data: updatedHOD });
+            }
+        }
+
+        if (!found)
+            return res.send({ error: "Sorry No department with this name" });
+
     } catch (err) {
         console.log('~ err', err);
         return res.send({ err: err });
--- server/models/Faculty.js
+++ server/models/Faculty.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-<<<<<<< HEAD
 
 const Department = require('./schemas/Department');
 
-=======
-const Department = require('./schemas/Department');
->>>>>>> 1e63b003725414970d49e095bf05e8bd9a3a60aa
 const FacultySchema = new Schema({
   name: {
     type: String,
