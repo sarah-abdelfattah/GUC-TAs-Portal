fd92395405458ae0fa550af0d0974e2f0ee1de35
Sarah Abdelfattah <sarah_23s@hotmail.com>
2020-12-20 07:49:29 +0200

    updated routes

--- server/routes/staffMembers.js
+++ server/routes/staffMembers.js
@@ -4,26 +4,14 @@ const auth = require('./auth');
 
 const staffMemberController = require('../controllers/staffMemberController');
 
-// function requiresAdmin(req, res, next) {
-//     console.log("OKKKKKKK")
-//     console.log(req.user);
-
-//     // if (req.user.admin !== true) {
-//     //     // res.status(401).end();
-//     // } else {
-//     //     next();
-//     // }
-// }
-
 //HR 
-router.post("/staff", staffMemberController.registerStaff);
-router.put("/staff", staffMemberController.updateStaff);
-router.delete("/staff", staffMemberController.deleteStaff);
+router.post("/staff", auth.HRAuth, staffMemberController.registerStaff);
+router.put("/staff", auth.HRAuth, staffMemberController.updateStaff);
+router.delete("/staff", auth.HRAuth, staffMemberController.deleteStaff);
 
 //all users
-// router.post("/logIn", staffMemberController.login);
 router.post("/logOut", staffMemberController.logout);
-router.post("/signIn", auth.AcademicMemberAuth, staffMemberController.signIn);
+router.post("/signIn", staffMemberController.signIn);
 router.post("/signOut", staffMemberController.signOut);
 
 router.post("/changePassword", staffMemberController.changePassword);
