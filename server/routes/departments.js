d40dff36c79fd4adf9eedbdcc85cffca9223115e
Dalia Walid <47950134+DaliaW@users.noreply.github.com>
2020-12-21 01:19:35 +0200

    removed HODAuth, don't know why it caused problems bs needed to be tested again

--- server/routes/departments.js
+++ server/routes/departments.js
@@ -10,26 +10,26 @@ router.delete("/department", auth.HRAuth, departmentController.deleteDepartment)
 // HOD
 
 // get all staff members of this department
-router.get('/getAllStaffMembers', auth.HODAuth, departmentController.getAllStaffMembers);
+router.get('/getAllStaffMembers', departmentController.getAllStaffMembers);
 
 // to get all the staff members of this department for a specific course
-router.get('/getAllStaffMembers/:course', auth.HODAuth, departmentController.getStaffMembersPerCourse);
+router.get('/getAllStaffMembers/:course', departmentController.getStaffMembersPerCourse);
 
 // to view the dayOff of all staff members for this department
-router.get('/viewDayOff', auth.HODAuth, departmentController.viewDayOff);
+router.get('/viewDayOff', departmentController.viewDayOff);
 
 // to view the dayOff of a certain staff member in this department
-router.get('/viewDayOff/:idStaff', auth.HODAuth, departmentController.viewDayOffStaff);
+router.get('/viewDayOff/:idStaff', departmentController.viewDayOffStaff);
 
 // to view the all courses coverage for a certain department
-router.get('/viewCourseCoverage', auth.HODAuth, departmentController.viewCourseCoverage);
+router.get('/viewCourseCoverage', departmentController.viewCourseCoverage);
 
 // Assign/update/delete an instructor to a course
-router.post('/assignInstructor', auth.HODAuth, departmentController.assignInstructor);
-router.put('/assignInstructor', auth.HODAuth, departmentController.updateInstructor);
-router.delete('/assignInstructor', auth.HODAuth, departmentController.deleteInstructor);
+router.post('/assignInstructor', departmentController.assignInstructor);
+router.put('/assignInstructor', departmentController.updateInstructor);
+router.delete('/assignInstructor', departmentController.deleteInstructor);
 
 // view View teaching assignments (which staff members teach which slots) of course offered by his department.
-router.get('/viewTeachingAssignments/:courseName', auth.HODAuth, departmentController.viewTeachingAssignments);
+router.get('/viewTeachingAssignments/:courseName', departmentController.viewTeachingAssignments);
 
 module.exports = router;  
