1d8a3a7e739f065232922c58ed7aee2f942f7507
Sarah Abdelfattah <sarah_23s@hotmail.com>
2020-12-27 17:12:04 +0200

    base environment

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//Import the pages
+
+function App() {
+  // eslint-disable-next-line
+  var currentLocation = window.location.pathname
+  // eslint-disable-next-line
+  var t = document.title = "GUC Portal"
+
+
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <Switch>
+            {/* <Route
+              exact
+              path="/login"
+              render={(props) => <Login {...props} />}
+            /> */}
+            {/* 
+            <Route exact path="/home" render={(props) => <Home {...props} />} />
+
+            <Route exact path="/" render={(props) => <Home {...props} />} /> */}
+          </Switch>
+
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
