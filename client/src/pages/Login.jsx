186579e72e241543f50e3ab61538e2c9f385e6fc
Dalia Walid <47950134+DaliaW@users.noreply.github.com>
2020-12-30 15:18:13 +0200

    WIP

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import setAuthToken from '../helpers/setAuthToken';
+import { link } from '../helpers/constants';
 
 function Login() {
 	const [gucId, setId] = useState("");
@@ -30,56 +31,56 @@ function Login() {
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 		const user = { gucId, password };
-		const response = await axios.post("http://localhost:5000/logIn", user);
-		console.log(response);
-		// if(response.err){
-		// 	// here we need to show toast and display err
-		// 	console.log(response.err);
-		// }
-		// else{
-		// 	setUser(response.header);
-		// 	// store the user in the localStorage
-		// 	const token = response.header.token
-		// 	localStorage.setItem('user', token);
-		// 	setAuthToken(token);
-		// 	document.location.href = '/home'
-		// 	console.log(token);
-		// }
+		const response = await axios.post(`${link}/logIn`, user);
+		console.log(response.data.token);
+		if(response.err){
+			// here we need to show toast and display err
+			console.log(response.err);
+		}
+		else{
+			setUser(response.header);
+			// store the user in the localStorage
+			const token = response.data.token
+			localStorage.setItem('user', token);
+			setAuthToken(token);
+			//document.location.href = '/home'
+			console.log(token);
+		}
 	};
 
 	return (
-		<div class="container-login100" style={{backgroundImage: "url('../assets/guc.jpg')"}}>
-			<div class="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
-				<form onSubmit={handleSubmit} class="login100-form validate-form">
-					<span class="login100-form-title p-b-37">
+		<div className="container-login100">
+			<div className="wrap-login100 p-l-55 p-r-55 p-t-80 p-b-30">
+				<form onSubmit={handleSubmit} className="login100-form validate-form">
+					<span className="login100-form-title p-b-37">
 						{greet}✨
 					</span>
 
-					<div class="wrap-input100 validate-input m-b-20" data-validate="Please enter your GUC ID">
+					<div className="wrap-input100 validate-input m-b-20" data-validate="Please enter your GUC ID">
 						<input 
 						value={gucId}
-						class="input100"
+						className="input100"
 						type="text"
 						name="text"
-						placeholder="guc id"
+						placeholder="guc id "
 						onChange = {({ target }) => setId(target.value)}
 						/>
-						<span class="focus-input100"></span>
+						<span className="focus-input100"></span>
 					</div>
 
-					<div class="wrap-input100 validate-input m-b-25" data-validate = "Please enter your password">
+					<div className="wrap-input100 validate-input m-b-25" data-validate = "Please enter your password">
 						<input
-						class="input100" 
+						className="input100" 
 						type="password" 
 						value={password}
 						placeholder="password"
 						onChange={({ target }) => setPassword(target.value)}
 						/>
-						<span class="focus-input100"></span>
+						<span className="focus-input100"></span>
 					</div>
 
-					<div class="container-login100-form-btn">
-						<button type="submit" class="login100-form-btn">
+					<div className="container-login100-form-btn">
+						<button type="submit" className="login100-form-btn">
 							Login In
 						</button>
 					</div>
