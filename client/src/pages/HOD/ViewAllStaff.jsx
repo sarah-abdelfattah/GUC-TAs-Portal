9810abf48a9c611ef56e7182ca95152143acb7c5
Dalia Walid <47950134+DaliaW@users.noreply.github.com>
2021-01-02 22:44:35 +0200

    finish HOD view Staff

--- client/src/pages/HOD/ViewAllStaff.jsx
+++ client/src/pages/HOD/ViewAllStaff.jsx
@@ -5,7 +5,7 @@ import Grid from "@material-ui/core/Grid";
 import { useToasts } from "react-toast-notifications";
 import axiosCall from "../../helpers/axiosCall";
 import { link } from "../../helpers/constants.js";
-import DetailPanel from "../../components/DetailPanel"
+import { Button } from "@material-ui/core";
 
 function ViewAllStaff() {
   const [data, setData] = useState([]); //table data
@@ -27,14 +27,14 @@ function ViewAllStaff() {
             `${link}/locations/room/all`
           );
           console.log(locations.data.data);
-          
+
           if (response.data.data.error) {
             addToast(response.data.data.error, {
               appearance: "warning",
               autoDismiss: true,
             });
           } else {
-            let data = response.data.data.map((staff) =>{
+            let data = response.data.data.map((staff) => {
               return {
                 name: staff.name,
                 gucId: staff.gucId,
@@ -42,14 +42,16 @@ function ViewAllStaff() {
                 email: staff.email,
                 role: staff.role,
                 salary: staff.salary,
-                location: locations.data.data.map((location) =>{
-                  if(staff.officeLocation === location._id){
-                    return location.location;
-                  }
-                  else return null;
-                }).filter((location) => location !== null)
-              }
-            })
+                id: staff._id,
+                location: locations.data.data
+                  .map((location) => {
+                    if (staff.officeLocation === location._id) {
+                      return location.location;
+                    } else return null;
+                  })
+                  .filter((location) => location !== null),
+              };
+            });
             setData(data);
           }
         } catch (err) {
@@ -60,54 +62,67 @@ function ViewAllStaff() {
       fetchData();
     }
   }, []);
-
+  
   return (
     // styling
-    <Grid container spacing={3}>
-      <Grid item xs={2}></Grid>
-      <Grid item xs={9}>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <MaterialTable
-          title=""
-          columns={[
-            {
-              title: "Avatar",
-              render: (rowData) => (
-                <Avatar
-                  maxInitials={1}
-                  size={40}
-                  round={true}
-                  name={rowData === undefined ? " " : rowData.name}
-                />
+    <div>
+      <Grid container spacing={3}>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={9}>
+          <br />
+          <br />
+          <MaterialTable
+            title=""
+            columns={[
+              {
+                title: "Avatar",
+                render: (rowData) => (
+                  <Avatar
+                    maxInitials={1}
+                    size={40}
+                    round={true}
+                    name={rowData === undefined ? " " : rowData.name}
+                  />
+                ),
+              },
+              { title: "Name", field: "name" },
+              { title: "Gender", field: "gender" },
+              { title: "ID", field: "gucId" },
+              { title: "Role", field: "role" },
+              { title: "Email", field: "email" },
+              { title: "office", field: "location" },
+            ]}
+            data={data}
+            actions={[
+              {
+                icon: "save",
+                tooltip: "Save User",
+                onClick: (event, rowData) => {
+                  document.location.href = `${rowData.id}/viewSchedule`;
+                },
+              },
+            ]}
+            options={{
+              actionsColumnIndex: -1,
+            }}
+            components={{
+              Action: (props) => (
+                <Button
+                  onClick={(event) => props.action.onClick(event, props.data)}
+                  color="primary"
+                  variant="contained"
+                  style={{ textTransform: "none" }}
+                  size="small"
+                >
+                  View schedule
+                </Button>
               ),
-            },
-            { title: "Name", field: "name" },
-            { title: "Gender", field: "gender" },
-            { title: "ID", field: "gucId" },
-            { title: "Role", field: "role" },
-            { title: "Email", field: "email" },
-            { title: "office", field: "location" },
-          ]}
-          data={data}
-          detailPanel={rowData => {
-            {console.log(rowData)}
-            return (
-              <div>
-              <DetailPanel 
-                data = {rowData.name}
-              />
-              </div>
-            )
-          }}
-          onRowClick={(event, rowData, togglePanel) => togglePanel()}
-        />
+            }}
+            //onRowClick={(event, rowData, togglePanel) => togglePanel()}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={1}></Grid>
-    </Grid>
+    </div>
   );
 }
 
