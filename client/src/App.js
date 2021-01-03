865fae1d828460e9fd84b4c32079abf8184a7a48
Dalia Walid <47950134+DaliaW@users.noreply.github.com>
2021-01-03 19:27:27 +0200

    added HOD view Course Coverage

--- client/src/App.js
+++ client/src/App.js
@@ -24,6 +24,7 @@ import Location from './pages/Location';
 import Faculty from "./pages/Faculty";
 import Department from "./pages/Department";
 import ViewAllStaff from './pages/HOD/ViewAllStaff';
+import ViewCourseCoverage from './pages/HOD/ViewCourseCoverage';
 
 
 function App() {
@@ -51,8 +52,8 @@ function App() {
                 <Route path='/location' render={(props) => <Location {...props} />} />
                 <Route path='/faculty' render={(props) => <Faculty {...props} />} />
                 <Route path='/department' render={(props) => <Department {...props} />} />
-                <Route exact path="/viewStaff" render={(props) => <ViewAllStaff {...props} />}
-                />
+                <Route exact path="/viewStaff" render={(props) => <ViewAllStaff {...props} />} />
+                <Route exact path="/viewCourseCoverage" render={(props) => <ViewCourseCoverage {...props} />} />
 
               </div>
             }
