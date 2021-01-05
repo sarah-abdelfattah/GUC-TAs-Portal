df0a9f80bb5c827e152a0c06e9af38b4c6a4a542
Sarah Abdelfattah <sarah_23s@hotmail.com>
2021-01-05 13:34:20 +0200

    finish view my attendance

--- client/src/components/AttendanceTable.jsx
+++ client/src/components/AttendanceTable.jsx
@@ -3,33 +3,66 @@ import MaterialTable from "material-table";
 import Grid from "@material-ui/core/Grid";
 import { useToasts } from "react-toast-notifications";
 import axiosCall from "../helpers/axiosCall";
-import { link } from "../helpers/constants.js";
-import { Button } from "@material-ui/core";
 import Fade from "react-reveal/Fade";
 import checkLogin from "../helpers/checkLogin";
-import TextField from "@material-ui/core/TextField";
-import Autocomplete from "@material-ui/lab/Autocomplete";
+import { IoFilter, IoCloseSharp } from "react-icons/io5";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Input,
+} from "@material-ui/core";
 
+//TODO: filter by year -->
+//year only with no month
+//remove this condition year only with no month
+//add condition both cannot be empty
 function AttendanceTable(props) {
   const [staff, setStaff] = useState([]);
   const [originalData, setOriginalData] = useState([]);
   const [data, setData] = useState([]); //table data
-  const [filtered, setFiltered] = useState([]);
+  const [filtered, setFiltered] = useState(false);
+  const [selectedMonth, setMonth] = useState("Month");
+  const [selectedYear, setYear] = useState("2021");
 
   const { addToast } = useToasts();
 
+  const year = [
+    "2021",
+    "2020",
+    "2019",
+    "2018",
+    "2017",
+    "2016",
+    "2015",
+    "2014",
+    "2013",
+    "2012",
+    "2011",
+    "2010",
+    "2009",
+    "2008",
+    "2007",
+    "2006",
+    "2005",
+    "2004",
+    "2003",
+  ];
+
   const month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "11 Jan - 10 Feb", //0 1-2
+    "11 Feb - 10 Mar", //1 2-3
+    "11 Mar - 10 Apr", //2 3-4
+    "11 Apr - 10 May", //3 4-5
+    "11 May - 10 Jun", //4 5-6
+    "11 Jun - 10 Jul", //5 6-7
+    "11 Jul - 10 Aug", //6 7-8
+    "11 Aug - 10 Sep", //7 8-9
+    "11 Sep - 10 Oct", //8 9-10
+    "11 Oct - 10 Nov", //9 10-11
+    "11 Nov - 10 Dec", //10 11-12
+    "11 Dec - 10 Jan", //11 12-1
   ];
 
   const compare = (a, b) => {
@@ -81,39 +114,49 @@ function AttendanceTable(props) {
     }
   }, [props]);
 
-  const handleFilter = async (inputData) => {
+  const handleFilter = async () => {
     try {
-      let term = parseInt(month.indexOf(inputData));
-      if (term > -1) {
-        let month1 = term + 1;
-        console.log(
-          "🚀 ~ file: AttendanceTable.jsx ~ line 89 ~ handleFilter ~ month1",
-          month1
-        );
-        let month2;
-        if (month1 === 12) month2 = 1;
-        else month2 = month1 + 1;
-        console.log(
-          "🚀 ~ file: AttendanceTable.jsx ~ line 93 ~ handleFilter ~ month2",
-          month2
-        );
-
-        // const res = await axiosCall(
-        //   "get",
-        //   `attendance/viewAttendance/${term}/${month2}`
-        // );
-        // console.log(
-        //   "🚀 ~ file: AttendanceTable.jsx ~ line 77 ~ handleFilter ~ res",
-        //   res.data
-        // );
-        // setFiltered(res.data);
+      if (selectedMonth === "Month") {
       } else {
-        addToast("Sorry this is not a valid month", {
-          appearance: "danger",
-          autoDismiss: true,
-        });
+        setFiltered(true);
+        let term = parseInt(month.indexOf(selectedMonth));
+        if (term > -1) {
+          let month1 = term + 1;
+          let month2;
+          if (month1 === 12) month2 = 1;
+          else month2 = month1 + 1;
+
+          const res = await axiosCall(
+            "get",
+            `attendance/viewAttendance/${month1}/${month2}`
+          );
+
+          addToast("filtered successfully", {
+            appearance: "success",
+            autoDismiss: true,
+          });
+
+          return setData(res.data);
+        } else {
+          addToast("Sorry this is not a valid month", {
+            appearance: "danger",
+            autoDismiss: true,
+          });
+        }
       }
-    } catch (error) {}
+    } catch (error) {
+      addToast("crashed", {
+        appearance: "danger",
+        autoDismiss: true,
+      });
+    }
+  };
+
+  const handleRemoveFilter = async () => {
+    setData(originalData);
+    setFiltered(false);
+    setMonth("");
+    setYear("2021");
   };
 
   return (
@@ -130,12 +173,6 @@ function AttendanceTable(props) {
                 {
                   title: "Date",
                   field: "date",
-                  // customFilterAndSearch: (term, rowData) => {
-                  //   if (term) {
-                  //     console.log("terrmmmm");
-                  //     handleFilter(term);
-                  //   }
-                  // },
                 },
                 { title: "Sign In", field: "startTime", filtering: false },
                 {
@@ -183,23 +220,47 @@ function AttendanceTable(props) {
               }}
               components={{
                 Toolbar: (props) => (
-                  <Autocomplete
-                    size="small"
-                    id="debug"
-                    options={month}
-                    onChange={(event, newValue) => {
-                      handleFilter(newValue);
-                    }}
-                    getOptionLabel={(option) => option}
-                    style={{ width: "30%", margin: "auto" }}
-                    renderInput={(params) => (
-                      <TextField
-                        {...params}
-                        label="View attendance of Month: "
-                        margin="normal"
+                  <div className="select-table-container">
+                    <Select
+                      className="table-select month"
+                      value={selectedMonth}
+                      onChange={(event) => setMonth(event.target.value)}
+                      placeholder="Month"
+                    >
+                      <MenuItem className="" value={"Month"} key={"Month"}>
+                        Month
+                      </MenuItem>
+                      {month.map((mon) => (
+                        <MenuItem className="" value={mon} key={mon}>
+                          {mon}
+                        </MenuItem>
+                      ))}
+                    </Select>
+
+                    <Select
+                      className="table-select year"
+                      value={selectedYear}
+                      onChange={(event) => setYear(event.target.value)}
+                      placeholder="Year"
+                    >
+                      {year.map((y) => (
+                        <MenuItem className="" value={y} key={y}>
+                          {y}
+                        </MenuItem>
+                      ))}
+                    </Select>
+
+                    <IoFilter
+                      className="filter-icon"
+                      onClick={() => handleFilter()}
+                    />
+                    {filtered ? (
+                      <IoCloseSharp
+                        className="filter-icon"
+                        onClick={handleRemoveFilter}
                       />
-                    )}
-                  />
+                    ) : null}
+                  </div>
                 ),
               }}
             />
